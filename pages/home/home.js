35d645f939ec7e749dea38a48f3241a56926b829
时上 <unknown@example.com>
2019-08-11 11:32:12 +0800

    删除了之前乱搞的代码。这个作为初始版

--- pages/home/home.js
+++ pages/home/home.js
@@ -42,41 +42,7 @@ Page({
     }
   },
   onShow:function(){
-    flag_hd = true;    //重新进入页面之后，可以再次执行滑动切换页面代码
-    clearInterval(interval); // 清除setInterval
-    time = 0;
-  },
-  // 触摸开始事件
-  touchStart: function (e) {
-    touchDot = e.touches[0].pageX; // 获取触摸时的原点
-    // 使用js计时器记录时间    
-    interval = setInterval(function () {
-      time++;
-    }, 100);
-  },
-  // 触摸结束事件
-  touchEnd: function (e) {
-    var touchMove = e.changedTouches[0].pageX;
-    // 向左滑动   
-    if (touchMove - touchDot <= -40 && time < 10 && flag_hd == true) {
-      flag_hd = false;
-      //执行切换页面的方法
-      console.log("向右滑动");
-      wx.switchTab({
-        url: '../right/right'
-      })
-    }
-    // 向右滑动   
-    if (touchMove - touchDot >= 40 && time < 10 && flag_hd == true) {
-      flag_hd = false;
-      //执行切换页面的方法
-      console.log("向左滑动");
-      wx.switchTab({
-        url: '../left/left'
-      })
-    }
-    clearInterval(interval); // 清除setInterval
-    time = 0;
+  
   },
   getUserInfo: function(e) {
     console.log(e)
